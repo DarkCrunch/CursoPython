58ececdfad5bf4ad6edf681d515279ee14746da4
DarkCrunch <73466295+DarkCrunch@users.noreply.github.com>
2021-08-15 15:01:16 -0500

    py

--- Python005EstructurasCondicionales.py
+++ Python005EstructurasCondicionales.py
@@ -1,8 +1,28 @@
 """
-Sentencia IF
+Sentencia IF simple
 ------------
 if condicion:
     print('condicion verdadera')
+else:
+    print('condicion falsa')
+
+Sentencia IF doble
+------------
+if condicion:
+    print('condicion verdadera')
+elif condicion:
+    print('condicion verdadera')
+else:
+    print('condicion falsa')
+
+Sentencia IF anidada
+------------
+if condicion:
+    print('condicion verdadera')
+    if condicion:
+        print('condicion verdadera')
+    else:
+        print('condicion falsa')
 else:
     print('condicion falsa')
 
